dcd05403023bea1de1436e2d48cd441bce484dbc
Nathan Minchow <nathan.minchow@outlook.com>
2018-08-29 16:21:14 -0500

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// create Oscillator node
+var oscillator = audioCtx.createOscillator();
+
+oscillator.type = 'square';
+oscillator.frequency.setValueAtTime(440, audioCtx.currentTime); // value in hertz
+oscillator.start();
+
+var toggleButton = document.getElementById("play");
+console.log(toggleButton);
+
+toggleButton.onclick = function() {
+    if (toggleButton.id === "play") {
+        oscillator.connect(audioCtx.destination);
+        toggleButton.id = "stop";
+        toggleButton.innerHTML = "Stop tone";
+    } else {
+        oscillator.disconnect(audioCtx.destination);
+        toggleButton.innerHTML = "Play tone";
+        toggleButton.id = "play";
+    }
+}
